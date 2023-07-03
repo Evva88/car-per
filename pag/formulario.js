e8249a9d05e9a880f7c2cd17b32cffcd9e56abd1
unknown <evavelli5@gmail.com>
2023-07-03 10:47:17 -0300

    form

--- pag/formulario.js
+++ pag/formulario.js
@@ -1,51 +1,45 @@
-class Cliente {
-    constructor(nombre, apellido, mail) {
-      this.nombre = nombre;
-      this.apellido = apellido;
-      this.mail = mail;
-    }
-  }
-  
-  const arrayClientes = [];
-  
-  const formulario = document.getElementById("formulario");
-  formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
-  
-    const nombre = document.getElementById("nombre");
-    const apellido = document.getElementById("apellido");
-    const mail = document.getElementById("mail");
-  
-    // Validación de formulario
-    if (nombre.value === "" || apellido.value === "" || mail.value === "") {
-      alert("Por favor complete todos los campos del formulario");
-      return;
-    }
-  
-    const cliente = new Cliente(nombre.value, apellido.value, mail.value);
-  
-    arrayClientes.push(cliente);
-    console.log(arrayClientes);
-  
-    
-    localStorage.setItem("arrayClientes", JSON.stringify(arrayClientes));
-  
-    formulario.reset();
-  });
-  
- 
-  let arrayClienteAgregado = JSON.parse(localStorage.getItem("arrayClientes"));
-  console.log(arrayClienteAgregado);
-  
-  function saludo() {
-    Swal.fire({
-      
-      icon: 'success',
-      title: 'En breve resonderemos tu mensaje',
-      showConfirmButton: false,
-      timer: 1500
-    });
- }
+// Importar la librería SweetAlert
 
- 
- document.getElementById("btn-enviar").addEventListener("click", saludo);
+
+// Obtener referencia al formulario
+const form = document.getElementById('myForm');
+
+// Agregar event listener al evento 'submit' del formulario
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevenir el envío predeterminado del formulario
+
+  // Validar el formulario
+  if (validateForm()) {
+    // Si el formulario es válido, redirigir al usuario a la dirección de correo electrónico especificada
+    window.location.href = 'mailto:evavelli5@gmail.com';
+  }
+});
+
+// Función para validar el formulario
+function validateForm() {
+  // Obtener referencia al campo de correo electrónico
+  const emailInput = document.getElementById('email');
+  const emailValue = emailInput.value.trim();
+
+  // Verificar si el campo de correo electrónico está vacío
+  if (emailValue === '') {
+    // Mostrar una alerta indicando que se debe ingresar una dirección de correo electrónico
+    alert('Por favor, ingresa tu dirección de correo electrónico.');
+    return false; // El formulario no es válido
+  }
+
+  // Mostrar la notificación de éxito utilizando SweetAlert
+  saludo();
+
+  return true; // El formulario es válido
+}
+
+// Función para mostrar la notificación de éxito
+function saludo() {
+  Swal.fire({
+    icon: 'success',
+    title: 'En breve responderemos tu mensaje',
+    showConfirmButton: false,
+    timer: 1500
+  });
+}
